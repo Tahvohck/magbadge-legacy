56394f7c5119877409cc57ff7df8d203406fc539
Tahvohck <Tahvohck@gmail.com>
2017-01-03 04:27:06 -0500

    More imports, more doc, expanded dummy data, start of actual request to api
    Note that api request is currently pointed at http://httpbin.org/post which
    echos the data sent to it.
    
    Dummy data expanded to include badge number.
    
    imported functools (needed for async kwargs) and requests (needed to talk to http)

--- badgeserv.py
+++ badgeserv.py
@@ -2,9 +2,12 @@ from datetime	import datetime
 from enum		import Enum
 from asyncio	import Task
 import asyncio
-import websockets
+import functools
 import json
+import requests
 import signal
+import websockets
+
 
 '''#####
  Global variables
@@ -19,12 +22,30 @@ DoW		= date.strftime("%A")
 pending	= {}
 actions = ["NULL",'RECON','BGCHK','CONFIG']
 connections	= {}
+# This dictionary defines the dummy reply when checking the badge "TEST"
 dummy_response = dict(
 	name		= dict(first="Edward", last="Richardson"),
 	badge		= "RRU-28413",
 	badge_t		= "staff",
+	badge_n		= 765,
 	hr_total	= 30,
-	hr_worked	= 0)
+	hr_worked	= 0
+)
+# Dictionary that defines the connection data that will later be exploded for requests.post()
+magapiopts = dict(
+	cert	= (	"",""),
+				#"magapi-client.crt",
+				#"magapi-client.key"),
+	#Thankfully we don't have to worry about Content-Length in Python (Hurrah!)
+	headers	= {	"Content-Type": "application/json"},
+	json	= {	"jsonrpc":	"2.0",
+				"method":	"barcode.lookup_attendee_from_barcode",
+				#"method":	"attendee.lookup",
+				"id":		"magbadgeserver-staffsuite"},
+#	url = "https://onsite.uber.magfest.org/jsonrpc/"
+	url = "https://httpbin.org/post"
+)
+
 
 '''#####
  Util functions
@@ -67,16 +88,26 @@ async def handleMessage(socket, path):
 	# Client Functionality
 	elif req_type == 'client':
 		cwt("New connection at {}:{}".format(socket.remote_address[0], socket.remote_address[1]))
+		magapiopts_lcl = magapiopts		#Create a connection-local copy of the request data
+
+		# As long as the connection is open, wait for and act on data
 		while socket.open:
 			try:
 				msg = await socket.recv()
 				msgParsed = json.loads(msg)
 				if msgParsed["action"] != actions[2]: raise KeyError()	#Check for malformation
 				cwt("Looking up badge ID: {}".format(msgParsed["BID"]))
+
 				if msgParsed["BID"] == "TEST":
 					cwt("Sending dummy data")
-					await socket.send(json.dumps(dummy_response, indent=2))
+					await socket.send(json.dumps(dummy_response, indent=2, sort_keys=True))
+
 				#TODO: Get an API key and write the request code
+				magapiopts_lcl['json']['params'] = msgParsed["BID"]
+				# Create a future that runs requests.post with the exploded copy of magapiopts as an argument.
+				future_response = loop.run_in_executor(None, functools.partial(requests.post, **magapiopts_lcl))
+				resp = await future_response
+
 			except websockets.exceptions.ConnectionClosed: pass
 			except KeyError: cwt("Malformed client message: \n{}".format(json.dumps(msgParsed, indent=2, sort_keys=True)))
 	# End Client Funtionality
